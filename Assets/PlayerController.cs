d1469a96decc56b7bb71fc1607a84925cc17c4a5
Pablo <pavel.konstantinov1@mail.ru>
2023-05-10 21:58:17 +0800

    movement update

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,29 +11,110 @@ public class PlayerController : MonoBehaviour
 
     Vector2 movementInput;
     Rigidbody2D rb;
+    Animator animator; 
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>(); 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>(); 
     }
 
     private void FixedUpdate()
     {
         if (movementInput != Vector2.zero)
         {
-            int count = rb.Cast(
+            bool success = TryMove(movementInput); 
+
+            if (!success)
+            {
+                success = TryMove(new Vector2(movementInput.x, 0));
+
+                if (!success)
+                {
+                    success = TryMove(new Vector2(0, movementInput.y)); 
+                }
+            }
+
+            animator.SetBool("isMoving", success);
+            rotatePlayer(movementInput); 
+            
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+    }
+
+    private void rotatePlayer(Vector2 direction)
+    {
+        Vector3 playerRotation = transform.eulerAngles; 
+
+        int zDirection = 0; 
+
+        if (direction.y > 0)
+        {
+            if (direction.x == 0)
+            {
+                zDirection = 0;
+            }
+            else if (direction.x > 0)
+            {
+                zDirection = -45;
+            }
+            else if (direction.x < 0)
+            {
+                zDirection = 45;
+            }
+        }
+        else if (direction.y < 0)
+        {
+            if (direction.x == 0)
+            {
+                zDirection = 180;
+            }
+            else if (direction.x > 0)
+            {
+                zDirection = -135;
+            }
+            else if (direction.x < 0)
+            {
+                zDirection = 135;
+            }
+        }
+        else if (direction.y == 0)
+        {
+            if (direction.x > 0)
+            {
+                zDirection = -90; 
+            }
+            else
+            {
+                zDirection = 90;
+            }
+        }
+
+        playerRotation.z = zDirection;
+        transform.eulerAngles = playerRotation; 
+    } 
+
+    private bool TryMove(Vector2 direction)
+    {
+        int count = rb.Cast(
                 movementInput, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
                 movementFilter, // The settings that determine where a collision can occur on such as layers to collide with
                 castCollisions, // List of collisions to store the found collisions into after the Cast is finished
                 moveSpeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
 
-            if (count == 0 )
-            {
-                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
-            }
-            
+        if (count == 0)
+        {
+            rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+            return true;
+        }
+        else
+        {
+            return false; 
         }
     }
 
